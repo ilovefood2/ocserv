4f41486ecab90b2df1efb1b0aa820a046ea399cd
ilovefood2 <4293869+ilovefood2@users.noreply.github.com>
2019-12-17 19:46:43 -0500

    Update ocserv_install.sh

--- radius/ocserv_install.sh
+++ radius/ocserv_install.sh
@@ -67,9 +67,9 @@ function centos1_ntp(){
 	sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 	yum -y install ntp
 	service ntpd restart
-	cp -rf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+	cp -rf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
 	cd /root
-	echo '0-59/10 * * * * /usr/sbin/ntpdate -u cn.pool.ntp.org' >> /tmp/crontab.back
+	echo '0-59/10 * * * * /usr/sbin/ntpdate -u ca.pool.ntp.org' >> /tmp/crontab.back
 	crontab /tmp/crontab.back
 	systemctl restart crond
 	yum install net-tools -y
@@ -79,14 +79,16 @@ function centos1_ntp(){
     yum install lynx wget expect iptables -y
 }
 function centos2_ocserv(){
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum install ocserv httpd -y
+yum install radiusclient-ng -y
 mkdir /root/anyconnect
 cd /root/anyconnect
 #Generate a CA certificate
 certtool --generate-privkey --outfile ca-key.pem
 cat >ca.tmpl <<EOF
-cn = "HY Annyconnect CA"
-organization = "HUAYU"
+cn = "Annyconnect CA"
+organization = "Annyconnect"
 serial = 1
 expiration_days = 3650
 ca
@@ -99,8 +101,8 @@ cp ca-cert.pem /etc/ocserv/
 #Generate a local server certificate
 certtool --generate-privkey --outfile server-key.pem
 cat >server.tmpl <<EOF
-cn = "HY Annyconnect CA"
-organization = "HUAYU"
+cn = "Annyconnect CA"
+organization = "Annyconnect"
 serial = 2
 expiration_days = 3650
 encryption_key
@@ -126,12 +128,12 @@ certtool --generate-crl --load-ca-privkey ca-key.pem \
 #Configuring ocserv
 cd /etc/ocserv/
 rm -rf ocserv.conf
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/ocserv.conf
+wget https://raw.githubusercontent.com/ilovefood2/ocserv/master/ocserv.conf
 #
 cd /root/anyconnect
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/gen-client-cert.sh
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/user_add.sh
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/user_del.sh
+wget https://raw.githubusercontent.com/ilovefood2/ocserv/master/gen-client-cert.sh
+wget https://raw.githubusercontent.com/ilovefood2/ocserv/master/user_add.sh
+wget https://raw.githubusercontent.com/ilovefood2/ocserv/master/user_del.sh
 chmod +x gen-client-cert.sh
 chmod +x user_add.sh
 chmod +x user_del.sh
